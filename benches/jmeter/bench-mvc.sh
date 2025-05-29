b2b619c0ef536497790187b61d3c53ffef4a94e4
BernardoPe <tgavent3@gmail.com>
2025-05-29 17:53:55 +0100

    test 4

--- benches/jmeter/bench-mvc.sh
+++ benches/jmeter/bench-mvc.sh
@@ -50,36 +50,36 @@ kill "$PID_MVC"
 wait $PID_GRADLE
 
 
-#cd ../../code || exit
-#
-#./gradlew runMVCVirtual -DbenchTimeout=1 -DXms=1024m -DXmx=16G > ../benches/jmeter/spring-mvc.log &
-#
-#PID_GRADLE=$!
-#
-#cd ../benches/jmeter || exit
-#
-#sleep 1
-#while ! grep -m1 'Tomcat started on port 8080' < spring-mvc.log; do
-#    sleep 1
-#done
-#
-#PID_MVC=$(grep -oP 'with PID \K[0-9]+' spring-mvc.log)
-#
-#echo ":::::::::::::::::::::::::::::::     Spring running PID = $PID_MVC"
-#echo ":::::::::::::::::::::::::::::::     Gradle running PID = $PID_GRADLE"
-#
-#echo "##########################################"
-#echo "############# RUN BENCH ##################"
-#echo "##########################################"
-#./run-jmeter.sh "${ROUTES[@]}" | tee spring-mvc-virtual-results.log
-#
-## Gracefully terminate the Spring Boot application when running on local machine.
-## It will send a SIGTERM corresponding to Exit code 143.
-#if [ "$GH" != "true" ]; then
-#  kill $PID_GRADLE
-#  kill "$PID_MVC"
-#
-#  # Wait for the process to exit
-#  wait $PID_GRADLE
-#fi
+cd ../../code || exit
+
+./gradlew runMVCVirtual -DbenchTimeout=1 -DXms=1024m -DXmx=16G > ../benches/jmeter/spring-mvc.log &
+
+PID_GRADLE=$!
+
+cd ../benches/jmeter || exit
+
+sleep 1
+while ! grep -m1 'Tomcat started on port 8080' < spring-mvc.log; do
+    sleep 1
+done
+
+PID_MVC=$(grep -oP 'with PID \K[0-9]+' spring-mvc.log)
+
+echo ":::::::::::::::::::::::::::::::     Spring running PID = $PID_MVC"
+echo ":::::::::::::::::::::::::::::::     Gradle running PID = $PID_GRADLE"
+
+echo "##########################################"
+echo "############# RUN BENCH ##################"
+echo "##########################################"
+./run-jmeter.sh "${ROUTES[@]}" | tee spring-mvc-virtual-results.log
+
+# Gracefully terminate the Spring Boot application when running on local machine.
+# It will send a SIGTERM corresponding to Exit code 143.
+if [ "$GH" != "true" ]; then
+  kill $PID_GRADLE
+  kill "$PID_MVC"
+
+  # Wait for the process to exit
+  wait $PID_GRADLE
+fi
 
