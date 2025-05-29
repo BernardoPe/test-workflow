71b889f0a78e39ba16e0c160a779e99f250b8cde
BernardoPe <tgavent3@gmail.com>
2025-05-29 20:02:54 +0100

    test 4

--- code/pssr-benchmark-controller/src/main/kotlin/benchmark/controller/presentations/reactive/PresentationsResourceReactive.kt
+++ code/pssr-benchmark-controller/src/main/kotlin/benchmark/controller/presentations/reactive/PresentationsResourceReactive.kt
@@ -11,35 +11,35 @@ import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
-@Path("/presentations/reactive")
-class PresentationsResourceReactive
-    @Inject
-    constructor(
-        private val presentations: PresentationRepository,
-    ) {
-        private val presentationsIter = presentations.findAllIterable()
-        private val presentationsFlux = presentations.findAllReactive()
-
-        @Location("qute/presentations")
-        lateinit var template: Template
-
-        @GET
-        @Path("/qute")
-        @Produces(MediaType.TEXT_HTML)
-        fun getQute(): Multi<String> {
-            return template.data("presentations", presentationsIter).createMulti()
-        }
-
-        @GET
-        @Path("/htmlFlow")
-        @Produces(MediaType.TEXT_HTML)
-        fun getHtmlFlow(): Multi<String> {
-            val view = AppendableMulti().also { sink ->
-                    PresentationsHtmlFlow.htmlFlowTemplate
-                        .writeAsync(sink, presentationsFlux)
-                        .thenAccept { sink.close() }
-                }
-            return view.toMulti()
-        }
-    }
-
+//@Path("/presentations/reactive")
+//class PresentationsResourceReactive
+//    @Inject
+//    constructor(
+//        private val presentations: PresentationRepository,
+//    ) {
+//        private val presentationsIter = presentations.findAllIterable()
+//        private val presentationsFlux = presentations.findAllReactive()
+//
+//        @Location("qute/presentations")
+//        lateinit var template: Template
+//
+//        @GET
+//        @Path("/qute")
+//        @Produces(MediaType.TEXT_HTML)
+//        fun getQute(): Multi<String> {
+//            return template.data("presentations", presentationsIter).createMulti()
+//        }
+//
+//        @GET
+//        @Path("/htmlFlow")
+//        @Produces(MediaType.TEXT_HTML)
+//        fun getHtmlFlow(): Multi<String> {
+//            val view = AppendableMulti().also { sink ->
+//                    PresentationsHtmlFlow.htmlFlowTemplate
+//                        .writeAsync(sink, presentationsFlux)
+//                        .thenAccept { sink.close() }
+//                }
+//            return view.toMulti()
+//        }
+//    }
+//
--- code/pssr-benchmark-controller/src/main/kotlin/benchmark/controller/presentations/reactive/StocksResourceReactive.kt
+++ code/pssr-benchmark-controller/src/main/kotlin/benchmark/controller/presentations/reactive/StocksResourceReactive.kt
@@ -11,34 +11,34 @@ import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
-@Path("/stocks/reactive")
-class StocksResourceReactive
-    @Inject
-    constructor(
-        stocks: StockRepository,
-    ) {
-        private val stocksIter = stocks.findAllIterable()
-        private val stocksFlux = stocks.findAllReactive()
-
-        @Location("qute/stocks")
-        lateinit var template: Template
-
-        @GET
-        @Path("/qute")
-        @Produces(MediaType.TEXT_HTML)
-        fun getQute(): Multi<String> {
-            return template.data("stocks", stocksIter).createMulti()
-        }
-
-        @GET
-        @Path("/htmlFlow")
-        @Produces(MediaType.TEXT_HTML)
-        fun getHtmlFlow(): Multi<String> {
-            val view = AppendableMulti().also { sink ->
-                    StocksHtmlFlow.htmlFlowTemplate
-                        .writeAsync(sink, stocksFlux)
-                        .thenAccept { sink.close() }
-                }
-            return view.toMulti()
-        }
-    }
+//@Path("/stocks/reactive")
+//class StocksResourceReactive
+//    @Inject
+//    constructor(
+//        stocks: StockRepository,
+//    ) {
+//        private val stocksIter = stocks.findAllIterable()
+//        private val stocksFlux = stocks.findAllReactive()
+//
+//        @Location("qute/stocks")
+//        lateinit var template: Template
+//
+//        @GET
+//        @Path("/qute")
+//        @Produces(MediaType.TEXT_HTML)
+//        fun getQute(): Multi<String> {
+//            return template.data("stocks", stocksIter).createMulti()
+//        }
+//
+//        @GET
+//        @Path("/htmlFlow")
+//        @Produces(MediaType.TEXT_HTML)
+//        fun getHtmlFlow(): Multi<String> {
+//            val view = AppendableMulti().also { sink ->
+//                    StocksHtmlFlow.htmlFlowTemplate
+//                        .writeAsync(sink, stocksFlux)
+//                        .thenAccept { sink.close() }
+//                }
+//            return view.toMulti()
+//        }
+//    }
